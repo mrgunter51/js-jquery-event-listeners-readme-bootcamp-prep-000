c6b920c9dd81af1ef85836f910ecbc7071bbf016
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-19 04:16:43 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function getIt(){
 }
 
 function frameIt(){
-  $('img').on('load', function(){
+  $('img').on('click', function(){
     var i = document.querySelector('img')
     i.className = "tasty"
   })
