21390d40d8700bc74178b5b4b50721a01bd52c4d
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-19 03:59:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,33 @@
 $(document).ready(function(){
 
 // call functions here
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
+})
 
-});
+function getIt(){
+  $('p').on('click', function(){alert("Hey!")})
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    var i = document.querySelector('img')
+    i.className = "tasty"
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown',function(key){
+    if(key.which === 71){
+      alert('G was pressed')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
